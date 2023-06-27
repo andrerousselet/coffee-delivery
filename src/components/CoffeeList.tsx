c99b6fe94a50c8fdb8294de055cfe3fc803eef64
Andre Rousselet <andre098@hotmail.com>
2023-06-27 16:56:19 -0300

    refactor(coffee list): add responsiveness to list of coffees

--- src/components/CoffeeList.tsx
+++ src/components/CoffeeList.tsx
@@ -7,7 +7,7 @@ export function CoffeeList() {
       <h2 className="font-baloo text-3xl font-extrabold text-base-title mb-14">
         Nossos cafés
       </h2>
-      <div className="pb-14 grid grid-cols-4 gap-8">
+      <div className="pb-14 grid grid-cols-1 place-items-center gap-8 sm:grid-cols-2 sm:gap-x-0 sm:gap-y-8 lg:grid-cols-4 lg:gap-8">
         {coffees.map((coffee) => (
           <CoffeeCard key={coffee.id} coffee={coffee} />
         ))}
