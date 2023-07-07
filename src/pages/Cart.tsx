dbbdf0a0da0e72a3c1156b365183874dbcb94a6c
Andre Rousselet <andre098@hotmail.com>
2023-07-07 10:52:30 -0300

    feat(cart page): add zod and hookform and function to set values into inputs when fetching

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,12 +1,59 @@
+import { FocusEvent, useState } from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Bank,
   CreditCard,
   CurrencyDollar,
   MapPinLine,
   Money,
+  SpinnerGap,
 } from "@phosphor-icons/react";
+import { fetchAddressInfo } from "../utils/fetchAddressInfo";
+
+const addressFormSchema = z.object({
+  zipcode: z.number().describe("Digite apenas números").min(8).max(8),
+  street: z.string(),
+  number: z.number(),
+  complement: z.string().optional(),
+  district: z.string(),
+  city: z.string(),
+  uf: z.string().toUpperCase(),
+});
+
+type AddressFormData = z.infer<typeof addressFormSchema>;
 
 export function Cart() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { register, setValue, setFocus, reset } = useForm<AddressFormData>({
+    resolver: zodResolver(addressFormSchema),
+  });
+
+  async function getAddressInfo(e: FocusEvent<HTMLInputElement>) {
+    const zipcode = e.target.value;
+    try {
+      if (zipcode) {
+        setIsLoading(true);
+        const address = await fetchAddressInfo(zipcode);
+        if (address) {
+          setValue("street", address.logradouro);
+          setValue("district", address.bairro);
+          setValue("city", address.localidade);
+          setValue("uf", address.uf);
+          setFocus("number");
+          setIsLoading(false);
+        }
+      } else {
+        reset();
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <main className="max-w-6xl mx-auto px-4">
       <div className="pt-10 flex items-start justify-between gap-8">
@@ -27,25 +74,40 @@ export function Cart() {
               </div>
             </div>
             <form className="flex flex-col gap-4">
-              <input
-                type="text"
-                placeholder="CEP"
-                className="w-[200px] p-3 rounded h-10 text-sm focus:outline-none focus:ring-1 focus:ring-yellow-dark placeholder:text-sm placeholder:text-base-label bg-base-input text-base-text border border-base-button focus:border-base-button"
-              />
+              <div className="flex items-center relative w-fit">
+                <input
+                  type="text"
+                  placeholder="CEP"
+                  {...register("zipcode", {
+                    valueAsNumber: true,
+                    onBlur: getAddressInfo,
+                  })}
+                  className="w-[200px] p-3 rounded h-10 text-sm focus:outline-none focus:ring-1 focus:ring-yellow-dark placeholder:text-sm placeholder:text-base-label bg-base-input text-base-text border border-base-button focus:border-base-button"
+                />
+                {isLoading && (
+                  <SpinnerGap
+                    size={20}
+                    className="animate-spin absolute right-3"
+                  />
+                )}
+              </div>
               <input
                 type="text"
                 placeholder="Rua"
+                {...register("street")}
                 className="p-3 rounded h-10 text-sm focus:outline-none focus:ring-1 focus:ring-yellow-dark placeholder:text-sm placeholder:text-base-label bg-base-input text-base-text border border-base-button focus:border-base-button"
               />
               <div className="flex gap-3">
                 <input
                   type="number"
                   placeholder="Número"
-                  className="w-[200px] p-3 rounded h-10 text-sm focus:outline-none focus:ring-1 focus:ring-yellow-dark placeholder:text-sm placeholder:text-base-label bg-base-input text-base-text border border-base-button focus:border-base-button"
+                  {...register("number")}
+                  className="w-[200px] p-3 rounded h-10 text-sm focus:outline-none focus:ring-1 focus:ring-yellow-dark placeholder:text-sm placeholder:text-base-label bg-base-input text-base-text border border-base-button focus:border-base-button [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                 />
                 <input
                   type="text"
                   placeholder="Complemento"
+                  {...register("complement")}
                   className="flex-1 p-3 rounded h-10 text-sm focus:outline-none focus:ring-1 focus:ring-yellow-dark placeholder:text-sm placeholder:text-base-label bg-base-input text-base-text border border-base-button focus:border-base-button"
                 />
               </div>
@@ -53,22 +115,25 @@ export function Cart() {
                 <input
                   type="text"
                   placeholder="Bairro"
+                  {...register("district")}
                   className="w-[200px] p-3 rounded h-10 text-sm focus:outline-none focus:ring-1 focus:ring-yellow-dark placeholder:text-sm placeholder:text-base-label bg-base-input text-base-text border border-base-button focus:border-base-button"
                 />
                 <input
                   type="text"
                   placeholder="Cidade"
+                  {...register("city")}
                   className="flex-1 p-3 rounded h-10 text-sm focus:outline-none focus:ring-1 focus:ring-yellow-dark placeholder:text-sm placeholder:text-base-label bg-base-input text-base-text border border-base-button focus:border-base-button"
                 />
                 <input
                   type="text"
                   placeholder="UF"
+                  {...register("uf")}
                   className="w-[60px] p-3 rounded h-10 text-sm focus:outline-none focus:ring-1 focus:ring-yellow-dark placeholder:text-sm placeholder:text-base-label bg-base-input text-base-text border border-base-button focus:border-base-button"
                 />
               </div>
             </form>
           </div>
-          <div className="bg-base-card p-10 rounded-md w-[640px] flex flex-col gap-8">
+          <div className="bg-base-card p-10 rounded-md w-[640px] flex flex-col gap-8 mb-3">
             <div className="flex gap-2">
               <CurrencyDollar size={22} className="text-purple" />
               <div>
@@ -114,3 +179,6 @@ export function Cart() {
     </main>
   );
 }
+
+// input type="number" without arrows
+// -> https://stackoverflow.com/questions/71296535/how-to-remove-arrow-on-input-type-number-with-tailwind-css
